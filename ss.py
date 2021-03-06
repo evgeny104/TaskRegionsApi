554d03a876b0f88efce4dc81704609151e263f0e
evgeny104 <80005043+evgeny104@users.noreply.github.com>
2021-03-06 06:09:14 +0300

    Удалить ss.py

--- ss.py
+++
@@ -1,9 +0,0 @@
-import json
-import requests
-
-def test_region_q():
-    res = requests.get('https://regions-test.2gis.com/1.0/regions?q=тау')
-    body = json.loads(res.text)
-    assert (body["items"][0]["name"]) == "Актау"
-
-test_region_q()
