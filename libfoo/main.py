3e1244e7bc5d76e1e1ab4e28859f8edbca4a260e
evgeny104 <nazawka88@gmail.com>
2021-03-06 05:44:02 +0300

    converting a json module to a string

--- libfoo/main.py
+++ libfoo/main.py
@@ -1,22 +1,22 @@
 import json
-from urllib import request
+import requests
 
 
 def test_total_count():     # Функция проверяет -строку- "total", общее количество регионов в базе 22
-    res = request.urlopen('https://regions-test.2gis.com/1.0/regions?page_size=5')
-    body = json.loads(res.read().decode("utf-8"))
+    res = requests.get('https://regions-test.2gis.com/1.0/regions?page_size=5')
+    body = json.loads(res.text)
     assert body["total"] == 22
 
 
 def test_region_q():        # Функция проверяет поиск по наванию региона
-    res = request.urlopen('https://regions-test.2gis.com/1.0/regions?q=тау')
-    body = json.loads(res.read().decode("utf-8"))
-    assert body["items"]["name"] == "Актау"
+    res = requests.get('https://regions-test.2gis.com/1.0/regions?q=тау')
+    body = json.loads(res.text)
+    assert (body["items"][0]["name"]) == "Актау"
 
 
 def test_contre_code():     # Функция проверяет фильтр по региону из всех достуных стран
-    res = request.urlopen('https://regions-test.2gis.com/1.0/regions?country_code=ru')
-    body = json.loads(res.read().decode("utf-8"))
+    res = requests.get('https://regions-test.2gis.com/1.0/regions?country_code=ru')
+    body = json.loads(res.text)
     assert body["items"][5]["country"]["name"] == "Россия"
     assert body["items"][0]["country"]["code"] == "ru"
 
